8bfb1bc531753398479a150616d82640aeafce45
Jacob Pudwill <jacobpudwill@gmail.com>
2023-10-02 21:56:30 -0500

    Add event handler for the 'appinstalled' event.

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -11,7 +11,24 @@ window.addEventListener('beforeinstallprompt', (event) => {
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+
+    if (!promptEvent) {
+    return;
+    }
+
+    // Show prompt
+    promptEvent.prompt();
+    
+    // Reset the deferred prompt variable
+    window.deferredPrompt = null;
+    
+    butInstall.classList.toggle('hidden', true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    // Clear prompt
+    window.deferredPrompt = null;
+});
